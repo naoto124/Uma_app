650250dc70462af6ce1c0137a99638065a8d4b6e
naoto124 <68520380+naoto124@users.noreply.github.com>
2020-09-01 21:44:07 +0900

    二回目

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "home#top"
 
+get "signup", to:"user#new", as:"singup"
+
 end
