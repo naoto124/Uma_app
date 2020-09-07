c610ff265f602907df0371ee994bf60d3834e4c7
naoto124 <68520380+naoto124@users.noreply.github.com>
2020-09-07 23:18:27 +0900

    password_digest2

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,19 @@
 class UserController < ApplicationController
 
-def new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(root_path)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:id,:name,:email,:password,:password_confirmation)
+  end
   
-end
 end
