f6b901b4c7ef91fd4c76d257a7ed32038a319b0d
naoto124 <68520380+naoto124@users.noreply.github.com>
2020-09-15 23:08:52 +0900

    画面作面

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
-
+before_action :current_user, {only: [:index]}
 
   def top
+
+  end
+
+  def index
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root "home#top"
-
+  
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   get "/signup", to:"user#new", as:"singup"
   post"/create",to:"user#create",as:"create"
 
+
+  get "/index", to:"home#index",as:"race_index"
+  root "home#top"  
 end
