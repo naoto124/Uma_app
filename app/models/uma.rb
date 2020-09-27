e0afbc6a1f5acef8c7a635e228da234a4972d016
naoto124 <68520380+naoto124@users.noreply.github.com>
2020-09-27 17:08:19 +0900

    スクレイピングurl繰り返し

--- app/models/uma.rb
+++ app/models/uma.rb
@@ -1,129 +1,54 @@
+require 'mechanize'
+
 class Uma < ApplicationRecord
   validates :name, {presence: true, uniqueness: true}
-  require 'mechanize'
 
-  def self.umas_index
-    agent = Mechanize.new
-    # page_a =('1'..'24').to_a
-    # page_ka =('1'..'21').to_a
-    # page_sa =('1'..'26').to_a
-    # page_ta =('1'..'18').to_a
-    # page_na =('1'..'6').to_a
-    # page_ha =('1'..'19').to_a
-    # page_ma =('1'..'16').to_a
-    # page_ya =('1'..'3').to_a
-    # page_ra =('1'..'16').to_a
-    # page_wa =('1'..'2').to_a
+  def self.umas_index_a_link
+      agent = Mechanize.new
 
+      texts = []
 
+      next_url = ""
 
-    def pages
+      while true
+          current_page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=0&page=#{next_url}")
+          next_page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=0&page=#{next_url + 1}")
 
-
-      kana = [a,ka,sa,ta,na,ha,ma,ya,ra,wa]
-
-      a.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=0&page=#{i}")
-        else
-          break
-        end
-      end
-      
-      ka.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=1&page=#{i}")
-        else
-          break
-        end
+          current_url = current_page.search('td b')
+          texts << current_url
+          next_url = current_url.text.to_i + 1
+          break unless next_page
       end
 
-      sa.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=2&page=#{i}")
-        else
-          break
-        end
-      end
-
-      ta.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=3&page=#{i}")
-        else
-          break
-        end
-      end
-
-      na.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=4&page=#{i}")
-        else
-          break
-        end
-      end
-
-      ha.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=5&page=#{i}")
-        else
-          break
-        end
-      end
-
-      ma.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=6&page=#{i}")
-        else
-          break
-        end
-      end
-
-      ya.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=7&page=#{i}")
-        else
-          break
-        end
-      end
-
-      ra.each do |i|
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=8&page=#{i}")
-        else
-          break
-        end
-      end
-
-      wa.each do {|i| i = 1++}
-        if page
-          page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=9&page=#{i}")
-        else
-          break
-        end
+      texts.each do |te|
+        get_uma("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=0&page=" + te)
       end
 
     end
 
+  def self.get_uma(te)
+      Mechanize.new
+      page = agent.get(te)
 
-    pages.each do |page|
+      def while
         elements = page.search('tr.white a')
         
         elements.each do |element|
           uma = Uma.new
           uma.name = element.inner_text
           uma.save
+        end
       end
-    end
+
+      def cream
+            elements = page.search('tr.cream a')
+            
+            elements.each do |element|
+              uma = Uma.new
+              uma.name = element.inner_text
+              uma.save
+            end
+        end
   end
 
-  # def self.umas_index_c
-  #   agent = Mechanize.new
-  #   page = agent.get("https://umanity.jp/racedata/database_search_horse.php?mode=1&c=0&page=1")
-  #   elements = page.search('tr.cream a')
-  #   elements.each do |element|
-  #     uma = Uma.new
-  #     uma.name = element.inner_text
-  #     uma.save
-  #   end
-  # end
 end
