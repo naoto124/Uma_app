9ba8f5a421771ac348d05d96af2a807b97b6e1de
naoto124 <68520380+naoto124@users.noreply.github.com>
2020-10-24 17:19:55 +0900

    これは動く、でも面倒です

--- app/controllers/uma_controller.rb
+++ app/controllers/uma_controller.rb
@@ -2,12 +2,12 @@
 class UmaController < ApplicationController
 
   def index
-    @umas = Uma.where('name like ?',"#{keyword}%").page(params[:page]).per(80)
+    @umas = Uma.where('name like ?',"ア%").page(params[:page]).per(80)
 
   end
 
   def create 
-    @keyword = params[:name]
+    # @keyword = params[:name]
 
   end
 end
