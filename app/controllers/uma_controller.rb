bb99fef31e8fe1388714e3dc0001f0e34758094d
naoto124 <68520380+naoto124@users.noreply.github.com>
2020-12-28 02:15:00 +0900

    整型中

--- app/controllers/uma_controller.rb
+++ app/controllers/uma_controller.rb
@@ -4,7 +4,25 @@ class UmaController < ApplicationController
   include UmaHelper
 
   def index
-    uma = Uma.all
+    @user_id = session[:user_id]
+    p session[:user_id]
+    p "eeeee"
+    @uma = Uma.all
+    @favorite = Favorite.where(user_id:@user_id)
+    @favorite_uma = []
+    @favorite.each do |f|
+      hash = Hash.new{|h,k| h[k] = uu }
+        if @uma.find {|u| u.id == f.uma_id}
+          u = Uma.find_by(id:f.uma_id)
+          id = f.uma_id
+          hash[:id] = id
+          hash.store(:name,u.name)
+          hash.store(:speed,f.speed)
+          hash.store(:power,f.power)
+        end
+        @favorite_uma << hash
+     end
+    @favorite_uma = Kaminari.paginate_array(@favorite_uma).page(params[:page]).per(30)
   end
 
   def create
